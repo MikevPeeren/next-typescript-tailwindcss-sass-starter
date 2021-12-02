ac60af6517cd500956d8bf57b6867a92c4db59c3
MikevPeeren <mikevpeeren@hotmail.com>
2021-12-02 20:58:39 +0100

    :lipstick: added tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+ purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: "class",
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+}
