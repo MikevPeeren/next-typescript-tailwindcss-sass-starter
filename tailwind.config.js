b45876a6bff492831585d9c05c46adf14b2423ad
MikevPeeren <mikevpeeren@hotmail.com>
2021-12-02 21:05:28 +0100

    :construction_worker: added husky prettier and lintstaged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   mode: "jit",
- purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: "class",
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
-}
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
