dbb72e40acc8b020e39605fcd052da7c3f7829c8
MikevPeeren <mikevpeeren@hotmail.com>
2022-10-30 19:48:48 +0100

    feature: added app directory as alternative

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Inter } from "@next/font/google";
+
+import "@/styles/global.scss";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter();
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <head>
+        <title>Mike van Peeren</title>
+      </head>
+      <body>
+        <div className="mx-auto max-w-screen-lg px-6 py-12">
+          <div className="col-start-2">
+            {/* Here you can place your Navigation */}
+          </div>
+          <div>{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
