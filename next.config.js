90efe19fb6c315b33f1f5e3cdcbd4e31383080ba
MikevPeeren <mikevpeeren@hotmail.com>
2023-05-22 14:24:23 +0200

    chore: removing experimental

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "@next/font/google";
 import "../../styles/globals.scss";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter();
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
