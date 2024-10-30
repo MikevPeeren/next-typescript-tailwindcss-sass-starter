7d95f7b57cfbd2079c6526f84541f004924eb265
MikevPeeren <mikevpeeren@hotmail.com>
2024-10-30 14:43:17 +0100

    fix: updating import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Inter } from "next/font/google";
 
-import "../../styles/globals.scss";
+import "@/styles/globals.scss";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
