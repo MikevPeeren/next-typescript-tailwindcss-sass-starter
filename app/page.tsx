d5db76d3d0437c63807ef9f81030bb27492a5526
MikevPeeren <mikevpeeren@hotmail.com>
2025-03-31 09:53:06 +0200

    chore: updating packages

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="m-0 bg-gradient-to-r from-blue-400 to-indigo-600 bg-clip-text text-center text-8xl font-extrabold text-transparent hover:cursor-pointer">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <p className="my-16 text-center text-2xl">
+        <p className="my-16 text-center text-lg">
           Get started by editing{" "}
           <code className="rounded-2xl bg-gray-100 p-3 text-base text-black">
             pages/index.js
@@ -16,8 +16,8 @@ export default function Home() {
             href="https://nextjs.org/docs"
             className="m-4 min-h-[200px] max-w-xs rounded-md border p-6 text-left no-underline transition-colors duration-700 ease-in-out hover:border-indigo-500 hover:text-indigo-300"
           >
-            <h2 className="mb-8 text-2xl">Documentation &rarr;</h2>
-            <p className="m-0 text-xl">
+            <h2 className="mb-8 text-lg">Documentation &rarr;</h2>
+            <p className="m-0 text-base">
               Find in-depth information about Next.js features and API.
             </p>
           </a>
@@ -26,8 +26,8 @@ export default function Home() {
             href="https://nextjs.org/learn"
             className="m-4 min-h-[200px] max-w-xs rounded-md border p-6 text-left no-underline transition-colors duration-700 ease-in-out hover:border-indigo-500 hover:text-indigo-300"
           >
-            <h2 className="mb-8 text-2xl">Learn &rarr;</h2>
-            <p className="m-0 text-xl">
+            <h2 className="mb-8 text-lg">Learn &rarr;</h2>
+            <p className="m-0 text-base">
               Learn about Next.js in an interactive course with quizzes!
             </p>
           </a>
@@ -36,8 +36,8 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="m-4 min-h-[200px] max-w-xs rounded-md border p-6 text-left no-underline transition-colors duration-700 ease-in-out hover:border-indigo-500 hover:text-indigo-300"
           >
-            <h2 className="mb-8 text-2xl">Examples &rarr;</h2>
-            <p className="m-0 text-xl">
+            <h2 className="mb-8 text-lg">Examples &rarr;</h2>
+            <p className="m-0 text-base">
               Discover and deploy boilerplate example Next.js projects.
             </p>
           </a>
@@ -46,8 +46,8 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="m-4 min-h-[200px] max-w-xs rounded-md border p-6 text-left no-underline transition-colors duration-700 ease-in-out hover:border-indigo-500 hover:text-indigo-300"
           >
-            <h2 className="mb-8 text-2xl">Deploy &rarr;</h2>
-            <p className="m-0 text-xl">
+            <h2 className="mb-8 text-lg">Deploy &rarr;</h2>
+            <p className="m-0 text-base">
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
